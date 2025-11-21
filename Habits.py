b7265c9340306bae4682fb7c8d83bb7c1ec16f51
arpan <arpanpatel284@gmail.com>
2025-11-21 14:09:17 -0600

    v2 saves to json file instead of text

--- Habits.py
+++ Habits.py
@@ -1,21 +1,8 @@
-import json
 from datetime import date
+import json
 import sys
 
-#function to write to a file
-def write_to_file(output):
-    with open('Habits.txt','a') as file:
-        file.write(output+"\n")
-
-#function to read file content
-def read_from_file():
-    with open('Habits.txt','r') as file:
-        lines = file.readlines()
-        if not lines:         
-            return None
-        return lines  
-
-def initialize_json(gym,water,study,pnl,pickleball):
+def create_entry(gym,water,study,pnl,pickleball):
     data = {
         "Date": date.today().isoformat(),
         "Gym": gym,
@@ -26,18 +13,6 @@ def initialize_json(gym,water,study,pnl,pickleball):
     }
     return data
     
-#print format template 
-def print_summary(entry):
-    
-    data = json.loads(entry)
-    print("\n===== Summary =====")
-    print(f"Date: {data['Date']}")
-    print(f"Gym: {'Yes' if data['Gym'] else 'No'}")
-    print(f"Water: {data['water']} L")
-    print(f"Study: {'Yes' if data['study'] else 'No'}")
-    print(f"Green P&L: {'Yes' if data['green P&L'] else 'No'}")
-    print(f"Pickleball: {'Yes' if data['Pickleball'] else 'No'}")
-    print("======================\n")
 
 #fucntion to get user input for streaks 
 def yes_or_no(prompt):
@@ -60,48 +35,72 @@ def ask_float(prompt):
         except ValueError:
             print("Please enter a valid number (e.g. 2 or 2.5)")
 
-#Create json object and populate it with user input and write to a file
-def log_habits():
+def load_db():
+    try:
+        with open("habits.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
 
+def save_db(data):
+    with open("habits.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+def print_all():
+    data = load_db()
+    if not data:
+        print("No entries found")
+        return
+
+    for entry in data:
+        printing_template(entry)
+
+def print_last_entry():
+    temp = load_db()
+    if not temp:
+        print("No entries found")
+        return 
+    
+    data = temp[-1]
+    printing_template(data)
+
+def printing_template(entry):
+
+    print("\n===== Summary =====")
+    print(f"Date: {entry['Date']}")
+    print(f"Gym: {'Yes' if entry['Gym'] else 'No'}")
+    print(f"Water: {entry['water']} L")
+    print(f"Study: {'Yes' if entry['study'] else 'No'}")
+    print(f"Green P&L: {'Yes' if entry['green P&L'] else 'No'}")
+    print(f"Pickleball: {'Yes' if entry['Pickleball'] else 'No'}")
+    print("======================\n")
+    
+
+def log_habits():
     gym = yes_or_no("Did you go to the Gym(Yes/No) ?")
     water = ask_float("How many liters of water did you drink?")
     study = yes_or_no("Did you study(Yes/No) ?")
     pnl = yes_or_no("Did you have a green day(Yes/No) ?")
     pickleball = yes_or_no("Did you go play pickleball(Yes/No) ?")
-    habits= initialize_json(gym, water, study, pnl, pickleball)
-    json_string = json.dumps(habits)
-    write_to_file(json_string)
-    return json_string
+    entry = create_entry(gym, water, study, pnl, pickleball)
+    db = load_db()
+    db.append(entry)
+    save_db(db)
+    return entry
 
-#Function to print the last entry 
-def print_last_entry():
-    lines = read_from_file()
-    if not lines:
-        print("No previous entries.")
-        return
-    last_entry = lines[-1].strip() 
-    print_summary(last_entry)
 
-#Fucntion to print the entire history
-def print_full_history ():
-    lines = read_from_file()
-    if not lines:
-        print("No Data")
-        return
-    for line in lines:
-        print_summary(line)
-
-# Defining main function
 def main():
     while True:
         function_selection = input("1. Log today\n2. View last entry\n3. View full history\n4. Exit\n")
         if(function_selection == "1"):
-            todays_habit = log_habits()
-            print_summary(todays_habit)
+            log_habits()
+            print_last_entry()
         elif(function_selection == "2"):
             print_last_entry()
         elif(function_selection == "3"):
-            print_full_history()
+            print_all()
         elif(function_selection == "4"):
             print("Have a great day!!")
             sys.exit()
